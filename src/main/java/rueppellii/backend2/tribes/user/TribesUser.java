40566436296773a4764ba4ff17fa0c048223c615
Bako Farkas <farkas.bako@gmail.com>
2019-01-21 16:12:59 +0100

    changed the names of the enviroment variables

--- src/main/java/rueppellii/backend2/tribes/user/TribesUser.java
+++ src/main/java/rueppellii/backend2/tribes/user/TribesUser.java
@@ -3,15 +3,13 @@ package rueppellii.backend2.tribes.user;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class TribesUser {
 
   @Id
