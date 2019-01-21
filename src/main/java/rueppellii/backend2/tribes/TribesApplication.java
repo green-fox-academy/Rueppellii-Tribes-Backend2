8697149a87c85d0cb99c5a88ea5b77cb9accad09
Gabor Molnar <giba86@gmail.com>
2019-01-21 10:34:34 +0100

    environment set up
    dependences added

---
+++ src/main/java/rueppellii/backend2/tribes/TribesApplication.java
@@ -0,0 +1,14 @@
+package rueppellii.backend2.tribes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TribesApplication.class, args);
+  }
+
+}
+
