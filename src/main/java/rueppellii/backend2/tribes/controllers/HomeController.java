28591a1eaa2f575e129d1dff907abdc6c042da5a
Lili <liliszendrei@gmail.com>
2019-01-21 11:22:17 +0100

    add hello + name

--- src/main/java/rueppellii/backend2/tribes/controllers/HomeController.java
+++ src/main/java/rueppellii/backend2/tribes/controllers/HomeController.java
@@ -1,13 +1,17 @@
 package rueppellii.backend2.tribes.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String helloWorld() {
-        return "Hello";
+    public String helloWorld(@RequestParam(required = false) String name) {
+        if (name == null) {
+            return "Hello";
+        }
+        return "Hello " + name;
     }
 }
