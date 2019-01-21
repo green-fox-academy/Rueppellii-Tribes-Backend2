3f6173f2177997497d2e9cece8376dbeb247eb6f
Lili <liliszendrei@gmail.com>
2019-01-21 16:42:41 +0100

    create kingdom class and repo

---
+++ src/main/java/rueppellii/backend2/tribes/kingdom/KingdomRepository.java
@@ -0,0 +1,6 @@
+package rueppellii.backend2.tribes.kingdom;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface KingdomRepository extends CrudRepository<Kingdom, Long> {
+}
