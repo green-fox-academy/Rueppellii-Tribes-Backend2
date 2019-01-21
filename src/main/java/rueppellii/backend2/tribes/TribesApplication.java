d5e7d1a1890fd3242565ba23ed6c3f9c660b591c
Lili <liliszendrei@gmail.com>
2019-01-21 14:21:12 +0100

    security commented out, test working

--- src/main/java/rueppellii/backend2/tribes/TribesApplication.java
+++ src/main/java/rueppellii/backend2/tribes/TribesApplication.java
@@ -3,15 +3,15 @@ package rueppellii.backend2.tribes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TribesApplication {
 
-  @Bean
-  public BCryptPasswordEncoder bCryptPasswordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+//  @Bean
+//  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//    return new BCryptPasswordEncoder();
+//  }
 
   public static void main(String[] args) {
     SpringApplication.run(TribesApplication.class, args);
