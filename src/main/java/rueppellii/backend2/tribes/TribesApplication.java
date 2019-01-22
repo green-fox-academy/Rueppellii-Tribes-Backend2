8103c65fd35671f2a8352e25fd5a965913537a07
Gabor Molnar <giba86@gmail.com>
2019-01-22 10:51:09 +0100

    TribesUserRepository findByUsername function tested successfully
    TribesUserService validation test failed

--- src/main/java/rueppellii/backend2/tribes/TribesApplication.java
+++ src/main/java/rueppellii/backend2/tribes/TribesApplication.java
@@ -2,16 +2,10 @@ package rueppellii.backend2.tribes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class TribesApplication {
 
-  @Bean
-  public BCryptPasswordEncoder bCryptPasswordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(TribesApplication.class, args);
--- src/main/java/rueppellii/backend2/tribes/user/TribesUserService.java
+++ src/main/java/rueppellii/backend2/tribes/user/TribesUserService.java
@@ -3,15 +3,32 @@ package rueppellii.backend2.tribes.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
 public class TribesUserService {
 
-  private TribesUserRepository userRepository;
+    private TribesUserRepository userRepository;
 
-  @Autowired
-  public TribesUserService(TribesUserRepository userRepository) {
-    this.userRepository = userRepository;
-  }
+    @Autowired
+    public TribesUserService(TribesUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
 
+    public void addUser(TribesUser tribesUser) throws IOException {
+        if(userExist(tribesUser.getUsername())) {
+            throw new IOException();
+        } else {
+            userRepository.save(tribesUser);
+        }
+    }
+
+    public boolean userExist(String username) {
+        return (userRepository.findByUsername(username) != null);
+    }
+
+    public TribesUser findUser(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
