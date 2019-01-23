51861c45d1aec9b024a66358913cbc95f765c59f
Gabor Molnar <giba86@gmail.com>
2019-01-23 10:20:10 +0100

    nothing to add

--- src/test/java/rueppellii/backend2/tribes/controllers/HomeControllerTest.java
+++ src/test/java/rueppellii/backend2/tribes/controllers/HomeControllerTest.java
@@ -1,6 +1,6 @@
 package rueppellii.backend2.tribes.controllers;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
