0d8de29044741dff555112aa5c08f60057a9776d
Gabor Molnar <giba86@gmail.com>
2019-01-24 21:00:37 +0100

    kingdom column removed from users
    version control for the second migration failed again. I need to know more for 2nd migration

--- src/main/resources/db/migration/mysql/V1_0__TribesMaster.sql
+++ src/main/resources/db/migration/mysql/V1_0__TribesMaster.sql
@@ -13,8 +13,13 @@ CREATE TABLE kingdoms (
  	application_user_user_id BIGINT,
 	PRIMARY KEY (id)
 );
+ ALTER TABLE kingdoms ADD CONSTRAINT fk_users_user FOREIGN KEY (application_user_user_id) REFERENCES users(user_id);
+ ALTER TABLE kingdoms ADD CONSTRAINT uq_users_kingdom UNIQUE (name);
+
 
 INSERT INTO users VALUES (1, 'TestUser1', 'pass1', 'user', 'UserCastle');
 INSERT INTO users VALUES (2, 'TestUser2', 'pass2', 'user', 'MockVillage');
 
 INSERT INTO kingdoms (name, application_user_user_id) SELECT kingdom, user_id FROM users;
+
+ALTER TABLE users DROP kingdom;
--- src/test/java/rueppellii/backend2/tribes/kingdom/KingdomRepositoryTest.java
+++ src/test/java/rueppellii/backend2/tribes/kingdom/KingdomRepositoryTest.java
@@ -1,16 +1,13 @@
 package rueppellii.backend2.tribes.kingdom;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @ActiveProfiles("Test")
 @DataJpaTest
 public class KingdomRepositoryTest {
