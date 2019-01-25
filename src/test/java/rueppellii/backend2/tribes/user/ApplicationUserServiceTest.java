06838e0bd2fa49810dda78766b7053f3dd8638fc
Gabor Molnar <giba86@gmail.com>
2019-01-25 15:07:04 +0100

    profiles set up

---
+++ src/test/java/rueppellii/backend2/tribes/user/ApplicationUserServiceTest.java
@@ -0,0 +1,54 @@
+package rueppellii.backend2.tribes.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import rueppellii.backend2.tribes.kingdom.KingdomService;
+import rueppellii.backend2.tribes.security.jwt.JwtProvider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("Test")
+@SpringBootTest
+class ApplicationUserServiceTest {
+
+    private ApplicationUserService applicationUserService;
+    private PasswordEncoder encoder;
+    private KingdomService kingdomService;
+    private JwtProvider jwtProvider;
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private ApplicationUserRepository applicationUserRepository;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        applicationUserService = new ApplicationUserService(applicationUserRepository, encoder, kingdomService, jwtProvider, authenticationManager);
+    }
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void existsByUsername() {
+    }
+
+    @Test
+    void saveApplicationUser() {
+    }
+
+    @Test
+    void authenticateApplicationUser() {
+    }
+}
