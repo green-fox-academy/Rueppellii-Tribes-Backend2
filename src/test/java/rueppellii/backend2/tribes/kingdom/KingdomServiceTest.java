7b1659799bb649e33f40437022490db7c73e05d7
csadesz <csadesz@gmail.com>
2019-01-25 14:29:16 +0100

    fix test

--- src/test/java/rueppellii/backend2/tribes/kingdom/KingdomServiceTest.java
+++ src/test/java/rueppellii/backend2/tribes/kingdom/KingdomServiceTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -14,7 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("Test")
-@SpringBootTest
 public class KingdomServiceTest {
 
     private KingdomService kingdomService;
