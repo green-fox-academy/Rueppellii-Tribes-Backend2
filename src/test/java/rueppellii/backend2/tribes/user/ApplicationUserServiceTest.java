dd7ab3049f5d05243e411c729ba71ab3a6b16ee4
Bako Farkas <farkas.bako@gmail.com>
2019-01-27 16:12:47 +0100

    test for uservalidity implemented

--- src/test/java/rueppellii/backend2/tribes/user/ApplicationUserServiceTest.java
+++ src/test/java/rueppellii/backend2/tribes/user/ApplicationUserServiceTest.java
@@ -1,12 +1,12 @@
 package rueppellii.backend2.tribes.user;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,8 +14,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import rueppellii.backend2.tribes.kingdom.KingdomService;
 import rueppellii.backend2.tribes.message.request.LoginForm;
+import rueppellii.backend2.tribes.message.request.SignUpForm;
+import rueppellii.backend2.tribes.message.response.SignUpResponse;
 import rueppellii.backend2.tribes.security.jwt.JwtProvider;
-import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(SpringExtension.class)
@@ -23,10 +26,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class ApplicationUserServiceTest {
 
+
     private ApplicationUserService applicationUserService;
+
+    @Mock
     private ApplicationUser applicationUser;
+
     private ResponseEntity response;
+
     private LoginForm loginForm;
+
+    private SignUpForm signUpForm;
+
+    private SignUpResponse signUpResponse;
+
 
     @Mock
     private PasswordEncoder encoder;
@@ -36,13 +49,13 @@ class ApplicationUserServiceTest {
     private JwtProvider jwtProvider;
     @Mock
     private AuthenticationManager authenticationManager;
-
-
     @Mock
     private ApplicationUserRepository applicationUserRepository;
 
     @BeforeEach
     public void init() {
+        signUpForm = new SignUpForm();
+        signUpResponse = new SignUpResponse();
         applicationUser = new ApplicationUser();
         MockitoAnnotations.initMocks(this);
         applicationUserService = new ApplicationUserService(applicationUserRepository, encoder, kingdomService, jwtProvider, authenticationManager);
@@ -50,17 +63,30 @@ class ApplicationUserServiceTest {
 
 
     @Test
-    void saveApplicationUserTestSuccess() {
+    void saveApplicationUserTest_WithKingdomName() throws Exception {
 
-
+        signUpForm.setUsername("TestUser");
+        signUpForm.setPassword("TestPW");
+        signUpForm.setKingdom("TestKingdom");
+        signUpResponse.setId(1L);
+        signUpResponse.setUsername("TestUser");
+        signUpResponse.setKingdomId(1L);
+        response = ResponseEntity.ok(signUpResponse);
+        when(applicationUserService.saveApplicationUser(signUpForm)).thenReturn(response);
     }
 
     @Test
-    void kingdomNameValidationTestSuccess() {
+    void saveApplicationUserTest_WithOUT_KingdomName() throws Exception {
+
+        signUpForm.setUsername("TestUser");
+        signUpForm.setPassword("TestPW");
+        signUpForm.setKingdom("");
+        signUpResponse.setId(1L);
+        signUpResponse.setUsername("TestUser");
+        signUpResponse.setKingdomId(1L);
+        response = ResponseEntity.ok(signUpResponse);
+        when(applicationUserService.saveApplicationUser(signUpForm)).thenReturn(response);
 
     }
 
-    @Test
-    void authenticateApplicationUserTestWithValidName() {
-    }
 }
