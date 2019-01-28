a34d68ac9dcc86b3bff6b6a4dda1ecbc8c992893
Bako Farkas <farkas.bako@gmail.com>
2019-01-28 11:31:16 +0100

    whyyyyyy

--- src/main/java/rueppellii/backend2/tribes/controller/AuthController.java
+++ src/main/java/rueppellii/backend2/tribes/controller/AuthController.java
@@ -2,11 +2,9 @@ package rueppellii.backend2.tribes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
-import rueppellii.backend2.tribes.exception.InvalidFieldException;
 import rueppellii.backend2.tribes.exception.UserNameIsTakenException;
 import rueppellii.backend2.tribes.exception.UserNotFoundException;
 import rueppellii.backend2.tribes.message.request.LoginForm;
