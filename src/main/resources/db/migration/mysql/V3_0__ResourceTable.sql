05a652bbc0992e887b2cf63fe520dc7c2a6115f4
aczender <czender.a@gmail.com>
2019-01-30 16:54:54 +0100

    Separated tables (kingdomresource)

--- src/main/resources/db/migration/mysql/V3_0__ResourceTable.sql
+++ src/main/resources/db/migration/mysql/V3_0__ResourceTable.sql
@@ -25,17 +25,31 @@ CREATE TABLE IF NOT EXISTS army (
 );
 
 CREATE TABLE resource (
-  resource_id BIGINT auto_increment,
+  resource_id BIGINT NOT NULL auto_increment,
   resource_type VARCHAR(255) UNIQUE,
   amount INT,
   updated_at TIMESTAMP,
   kingdom_id BIGINT,
   PRIMARY KEY (resource_id)
 );
-ALTER TABLE kingdoms ADD resource_id BIGINT;
 
-ALTER TABLE kingdoms ADD CONSTRAINT fk_resource_resource_id
-  FOREIGN KEY (resource_id) REFERENCES resource(resource_id);
+CREATE TABLE kingdomresource (
+  kingdom_id BIGINT,
+  resource_id BIGINT,
+  PRIMARY KEY (resource_id),
+  KEY resource_id (resource_id),
+  CONSTRAINT fk_kingdoms FOREIGN KEY (kingdom_id) REFERENCES kingdoms(id),
+  CONSTRAINT fk_resource FOREIGN KEY (resource_id) REFERENCES resource(resource_id)
+);
 
-  INSERT INTO resource VALUES (3, 'gold', 20, null , 2);
+INSERT INTO users VALUES (3, 'TestUsasder1', 'passas1', 'useasdr');
+INSERT INTO users VALUES (4, 'TestUasdser2', 'passfds2', 'usewqer');
+INSERT INTO kingdoms VALUES (3, 'Testking', 3, null);
+INSERT INTO resource VALUES (1, 'gold', 20, null, 2);
+
+INSERT INTO kingdomresource (resource_id, kingdom_id) SELECT resource_id, kingdom_id FROM resource;
+
+
+
+
 
