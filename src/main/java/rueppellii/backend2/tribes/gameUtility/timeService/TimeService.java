398a47300c73e381728c35f8e4daa9cb9c628040
aczender <czender.a@gmail.com>
2019-02-11 10:19:37 +0100

    Merge branch 'TR2-31-Archie-2' of https://github.com/green-fox-academy/Rueppellii-Tribes-Backend2 into TR2-20
    # Conflicts:
    #	src/main/java/rueppellii/backend2/tribes/gameUtility/purchaseService/PurchaseService.java
    #	src/main/java/rueppellii/backend2/tribes/gameUtility/timeService/TimeConstants.java
    #	src/main/java/rueppellii/backend2/tribes/gameUtility/timeService/TimeService.java
    #	src/main/java/rueppellii/backend2/tribes/gameUtility/timeService/TimeServiceImpl.java
    #	src/main/java/rueppellii/backend2/tribes/resource/presistence/model/Food.java
    #	src/main/java/rueppellii/backend2/tribes/resource/presistence/model/Gold.java
    #	src/main/java/rueppellii/backend2/tribes/resource/presistence/model/Resource.java
    #	src/main/java/rueppellii/backend2/tribes/resource/service/ResourceService.java

--- src/main/java/rueppellii/backend2/tribes/gameUtility/timeService/TimeService.java
+++ src/main/java/rueppellii/backend2/tribes/gameUtility/timeService/TimeService.java
@@ -8,15 +8,10 @@ import java.sql.Timestamp;
 public interface TimeService {
 
     Long calculateTimeOfBuildingCreation(Kingdom kingdom);
-
     Long calculateTimeOfBuildingUpgrade(Kingdom kingdom);
-
     Long calculateTimeOfTroopCreation(Kingdom kingdom);
-
     Long calculateTimeOfTroopUpgrade(Kingdom kingdom);
-
     Boolean timeIsUp(ProgressionModel progressionModel);
-
     Long timeDifference(Timestamp currentTime, Timestamp updatedAtTime);
 
 }
