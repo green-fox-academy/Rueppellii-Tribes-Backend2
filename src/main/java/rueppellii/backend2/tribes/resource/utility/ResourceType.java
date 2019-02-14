dbf8256492b509492520d0095b3d8213fa9daa4e
giba86 <giba86@gmail.com>
2019-02-14 10:21:53 +0100

    resource enum builder function changed to abstract

--- src/main/java/rueppellii/backend2/tribes/resource/utility/ResourceType.java
+++ src/main/java/rueppellii/backend2/tribes/resource/utility/ResourceType.java
@@ -7,19 +7,19 @@ import rueppellii.backend2.tribes.resource.presistence.model.Resource;
 
 public enum ResourceType {
     GOLD {
+        @Override
         public Resource produceResource() {
             return new Gold();
         }
     },
     FOOD {
+        @Override
         public Resource produceResource() {
             return new Food();
         }
     };
 
-    public Resource produceResource() {
-        return null;
-    }
+    public abstract Resource produceResource();
 
     public String getName(){
         return this.name();
