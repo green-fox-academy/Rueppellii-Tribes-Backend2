dbec64cd914302736f220c3b41974435200fb967
giba86 <giba86@gmail.com>
2019-02-14 15:03:27 +0100

    EnumTuplizer added

--- src/main/java/rueppellii/backend2/tribes/user/util/Role.java
+++ src/main/java/rueppellii/backend2/tribes/user/util/Role.java
@@ -1,11 +1,17 @@
 package rueppellii.backend2.tribes.user.util;
 
+import org.hibernate.annotations.Tuplizer;
+import rueppellii.backend2.tribes.enumToTable.EnumTuplizer;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "roles")
+@Tuplizer(impl = EnumTuplizer.class)
 public enum Role {
+    @Column(name = "role_enum")
     ADMIN, USER;
 
     public String authority() {
--- src/main/java/rueppellii/backend2/tribes/user/util/RoleEnumUserType.java
+++ src/main/java/rueppellii/backend2/tribes/user/util/RoleEnumUserType.java
@@ -1,10 +1,10 @@
-package rueppellii.backend2.tribes.user.util;
-
-import rueppellii.backend2.tribes.enumToTable.EnumUserType;
-
-public class RoleEnumUserType extends EnumUserType<Role> {
-
-    public RoleEnumUserType(Class<RoleEnumUserType> c) {
-        super(Role.class);
-    }
-}
+//package rueppellii.backend2.tribes.user.util;
+//
+//import rueppellii.backend2.tribes.enumToTable.EnumUserType;
+//
+//public class RoleEnumUserType extends EnumUserType<Role> {
+//
+//    public RoleEnumUserType(Class<RoleEnumUserType> c) {
+//        super(Role.class);
+//    }
+//}
