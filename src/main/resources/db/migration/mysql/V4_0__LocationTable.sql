89d8e3b248f3f9d63742724b5bb585328a9ec0f3
aczender <czender.a@gmail.com>
2019-02-25 15:49:08 +0100

    LocationTable added to flyway

--- src/main/resources/db/migration/mysql/V4_0__LocationTable.sql
+++ src/main/resources/db/migration/mysql/V4_0__LocationTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS locations (
   id BIGINT auto_increment,
-  location VARCHAR (20),
+  country VARCHAR (20),
   PRIMARY KEY (id),
   FOREIGN KEY (kingdom_id) REFERENCES kingdoms(id)
 );
