4e31813d48d56754a1c172d6b40949badc860bad
Lili <liliszendrei@gmail.com>
2019-02-27 17:21:11 +0100

    delete unused imports

--- src/main/java/rueppellii/backend2/tribes/progression/persistence/ProgressionModelRepository.java
+++ src/main/java/rueppellii/backend2/tribes/progression/persistence/ProgressionModelRepository.java
@@ -1,13 +1,10 @@
 package rueppellii.backend2.tribes.progression.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import rueppellii.backend2.tribes.kingdom.persistence.model.Kingdom;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ProgressionModelRepository extends JpaRepository<ProgressionModel, Long> {
