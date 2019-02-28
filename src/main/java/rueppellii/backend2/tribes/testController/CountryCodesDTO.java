6d1ba037d3dd40c982e97a25f16c9e4add98fd4b
Lili <liliszendrei@gmail.com>
2019-02-28 17:02:02 +0100

    connect to outer api

---
+++ src/main/java/rueppellii/backend2/tribes/testController/CountryCodesDTO.java
@@ -0,0 +1,13 @@
+package rueppellii.backend2.tribes.testController;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class CountryCodesDTO {
+
+    private List<String> countryCodes;
+}
