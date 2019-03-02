ead33a0dcd534124fdcb376701d215ba532c3b2f
giba86 <giba86@gmail.com>
2019-03-02 21:10:44 +0100

    unneccessary classes and dependences removed as well as build folder; now only flyway schemas, and test file changes are added

--- build/resources/main/db/migration/mysql/V3_0__ProgressionTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS progression (
-  id BIGINT auto_increment,
-  type VARCHAR(255),
-  game_object_id BIGINT,
-  time_to_progress BIGINT,
-  kingdom_id BIGINT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (kingdom_id) REFERENCES kingdoms(id)
-);
--- build/resources/main/db/migration/mysql/V4_0__LocationTable.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS locations (
-  id BIGINT auto_increment,
-  country VARCHAR (20),
-  PRIMARY KEY (id),
-  FOREIGN KEY (kingdom_id) REFERENCES kingdoms(id)
-);
