371787dc05d53a5d12c06ce1d08c10c4bff468b2
Lili <liliszendrei@gmail.com>
2019-03-05 09:57:17 +0100

    fix import

--- src/main/java/rueppellii/backend2/tribes/kingdom/service/KingdomService.java
+++ src/main/java/rueppellii/backend2/tribes/kingdom/service/KingdomService.java
@@ -24,6 +24,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
