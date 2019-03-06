ddbaed92afffc235a2e4c7abbbccfc330aee5c4a
aczender <czender.a@gmail.com>
2019-03-06 14:24:58 +0100

    BuildingServiceTest first commit

---
+++ src/test/java/rueppellii/backend2/tribes/building/service/BuildingServiceTest.java
@@ -0,0 +1,128 @@
+package rueppellii.backend2.tribes.building.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import rueppellii.backend2.tribes.building.exception.BuildingNotFoundException;
+import rueppellii.backend2.tribes.building.persistence.model.Building;
+import rueppellii.backend2.tribes.building.utility.BuildingType;
+import rueppellii.backend2.tribes.kingdom.persistence.model.Kingdom;
+import rueppellii.backend2.tribes.progression.persistence.ProgressionModel;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static rueppellii.backend2.tribes.building.utility.BuildingFactory.makeBuilding;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("Test")
+@SpringBootTest
+@SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:clear.sql"), @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:data.sql"), @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD, scripts = "classpath:clear.sql")})
+class BuildingServiceTest {
+
+    @Autowired
+    private BuildingService buildingService;
+
+    private Kingdom testKingdom;
+    private Building testBuilding;
+    private Long buildingId;
+    private String buildingType;
+    private int buildingLevel;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void findBuildingById_Test() throws BuildingNotFoundException {
+        buildingId = 2L;
+        testBuilding = buildingService.findById(buildingId);
+        buildingLevel = testBuilding.getLevel();
+
+        Assertions.assertEquals(buildingLevel, 1);
+    }
+
+
+    @Transactional
+    void findBuildingInKingdom_Test() throws BuildingNotFoundException {
+        buildingId = 1L;
+        testBuilding = buildingService.findById(buildingId);
+        testKingdom = testBuilding.getBuildingsKingdom();
+//        Building testBuilding = buildingService.findBuildingInKingdom(testKingdom, buildingId);
+
+        Assertions.assertEquals(3, testBuilding);
+    }
+
+//    @Test
+//    void isTownhall() {
+//
+//    }
+
+//    @Test
+//    void validateBuildingUpgrade() {
+//    }
+//
+
+    @Test
+    @Transactional
+    void getLevelOfTownHall() throws BuildingNotFoundException {
+        buildingId = 2L;
+        testBuilding = buildingService.findById(buildingId);
+        buildingType = testBuilding.getType().getName();
+        buildingLevel = testBuilding.getLevel();
+
+        Assertions.assertEquals(buildingLevel, 1);
+
+    }
+
+    @Test
+    @Transactional
+    void createBuilding_Test() throws BuildingNotFoundException, IllegalArgumentException {
+        ProgressionModel progressionModel = new ProgressionModel();
+        progressionModel.setId(1L);
+        progressionModel.setType("Farm");
+        progressionModel.setGameObjectId(9L);
+        progressionModel.setTimeToProgress(1551488461000L);
+        buildingService.createBuilding(progressionModel, testKingdom);
+        Long expectedBuildingId = 9L;
+        Building createdBuilding = buildingService.findById(expectedBuildingId);
+
+        Assertions.assertNotNull(createdBuilding);
+    }
+
+    @Test
+    void checkIfBuildingIsUnderTownhallLevel() {
+    }
+
+    @Test
+    void checkIfBuildingIsUnderMaxLevel() {
+    }
+
+    @Test
+    void validateBuildingUpgrade() {
+    }
+
+    @Test
+    @Transactional
+    public void upgradeBuilding_Test() throws BuildingNotFoundException {
+        ProgressionModel progressionModel = new ProgressionModel();
+        progressionModel.setId(2L);
+        progressionModel.setGameObjectId(2L);
+        progressionModel.setTimeToProgress(1551488461000L);
+        buildingService.upgradeBuilding(progressionModel, testKingdom);
+
+        int buildingLevel = buildingService.findById(2L).getLevel();
+
+        Assertions.assertEquals(java.util.Optional.ofNullable(2), java.util.Optional.ofNullable(buildingLevel));
+    }
+}
