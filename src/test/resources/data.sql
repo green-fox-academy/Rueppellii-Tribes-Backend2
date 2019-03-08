506aa4ddabb2c08deb77630301b623e3fb808558
Lili <liliszendrei@gmail.com>
2019-03-08 13:17:11 +0100

    fix test sql database

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -20,14 +20,14 @@ INSERT INTO resources(id, type, dtype, amount, updated_at, resource_per_minute, 
          (4, 'FOOD', 'Food', 50, NULL, 5, 2);
 
 INSERT INTO buildings(id, type, level, hp, kingdom_id)
-  VALUES (1, 'Townhall', 1, 10, 1),
+  VALUES (1, 'TownHall', 1, 10, 1),
          (2, 'Farm', 1, 1, 1),
          (3, 'Mine', 1, 5, 1),
-         (4, 'Barrack', 1, 100, 1),
-         (5, 'Townhall', 1, 10, 2),
+         (4, 'Barracks', 1, 100, 1),
+         (5, 'TownHall', 1, 10, 2),
          (6, 'Farm', 1, 1, 2),
          (7, 'Mine', 1, 5, 2),
-         (8, 'Barrack', 1, 100, 2);
+         (8, 'Barracks', 1, 100, 2);
 
 INSERT INTO troops (id, level, HP, attack, defense, kingdom_id)
   VALUES (1, 1, 10, 1, 1, 1),
